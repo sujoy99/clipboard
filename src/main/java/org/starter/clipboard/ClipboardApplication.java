14fead7e76da536e776e0ef59c947cc51158e68b
sujoy99 <scg.sujoy@gmail.com>
2023-05-17 02:40:29 +0600

    init

---
+++ src/main/java/org/starter/clipboard/ClipboardApplication.java
@@ -0,0 +1,13 @@
+package org.starter.clipboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClipboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClipboardApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/starter/clipboard/controller/MainController.java
@@ -0,0 +1,42 @@
+package org.starter.clipboard.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class MainController {
+
+    Logger logger = LoggerFactory.getLogger(MainController.class);
+
+    @GetMapping("/")
+    public String getForm(){
+        logger.info("::----get controller-----::");
+        return "index";
+    }
+
+    @PostMapping("/upload")
+    public String uploadFiles(@RequestParam("files") MultipartFile[] files) {
+        logger.info("::----post controller-----::");
+        String message = "";
+        try {
+            Arrays.asList(files).stream().forEach(file -> {
+                logger.info("::----file name-----::", file.getOriginalFilename());
+            });
+
+            message = "Uploaded the files successfully: ";
+            return message;
+        } catch (Exception e) {
+            message = "Fail to upload files!";
+            return message;
+        }
+    }
+}
